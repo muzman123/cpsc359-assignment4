78709b0da321bc9cd099ac90b391dfb5e7656ec8
Ammittoje Singh <146037485+singh-ammittoje@users.noreply.github.com>
2024-04-01 13:55:53 -0600

    Update level.c

--- 359workingGame/snake/source/level.c
+++ 359workingGame/snake/source/level.c
@@ -208,103 +208,172 @@ void drawLevel1(){
 	drawImage(finish.pixel_data,finish.width,finish.height,19*32,19*32);
 	gameStage.mapper[19][19] = 3;
 }
-/*
 // LEVEL 2 DRAW METHOD
 void drawLevel2(){
 
-	gameStage.currentLevel = 1;
-	for( int k = 0; k<22;k++){
+	//background
+	for( int i = 0; i < 20;i++){
 		for(int j = 0; j<20;j++){
-			drawImage(j*32, k*32, &tilemap.images[grass]);
-			gameStage.mapper[k][j] = 0;
+			drawImage(grass.pixel_data,grass.width,grass.height,i*32, j*32);
 		}
 	}
-
-
+    
+    // water (stationary enemy)
 	for(int i = 0; i < 3;i++){
-	for(int j = 0; j<20; j++){
-		drawImage(j*32, i*32, &tilemap.images[water]);
-		gameStage.mapper[i][j] = 1;
-	}
-	}
-	for(int j = 38; j < 20;j++){
-		for(int i = 0; i<22; i++){
-			drawImage(j*32, i*32, &tilemap.images[water]);
+		for(int j = 0; j < 40; j++){
+			drawImage(water.pixel_data,water.width,water.height,i*32, j*32);
 			gameStage.mapper[i][j] = 1;
 		}
 	}
-	for(int i = 0; i < 3;i++){
-		for(int j = 0; j<21; j++){
-			drawImage(i*32, j*32, &tilemap.images[water]);
-			gameStage.mapper[j][i] = 1;
+	for(int j = 38; j < 40;j++){
+		for(int i = 0; i < 22; i++){
+			drawImage(water.pixel_data,water.width,water.height,i*32, j*32);
+			gameStage.mapper[i][j] = 1;
 		}
 	}
-
+	for(int j = 0; j < 21;j++){
+		drawImage(water.pixel_data,water.width,water.height,19*32, j*32);
+		gameStage.mapper[3][j] = 1;
+	}
 	for(int i = 1; i < 39;i++){
-		drawImage(i*32, 64, &tilemap.images[water]);
-		gameStage.mapper[2][i] = 1;
+		drawImage(water.pixel_data,water.width,water.height,10*32, j*32);
+		gameStage.mapper[i][2] = 1;
 	}
 
-	for(int j = 25; j< 38;j++){
-		drawImage(j*32, (19*32), &tilemap.images[water]);
-		gameStage.mapper[19][j] = 1;
-	}
-
-	for(int j = 0; j< (gameStage.lives);j++){
-		drawImage(0, j*32, &tilemap.images[heart]);
-	}
-
-	int j = 21;
-	for(int i = 0; i < 20;i++){
-		drawImage(i*32, j*32, &tilemap.images[water]);
-		gameStage.mapper[j][i] = 1;
-		drawImage(i*32, (j-1)*32, &tilemap.images[water]);
-		gameStage.mapper[j-1][i] = 1;
-	}
-
-	// DRAW PLATFORMS
-
-	gameStage.starti = 3*32;
-	gameStage.startj = 24*32;
-	for(int j = 3; j < 25; j++){
-		drawImage(j*32, (4)*32, &tilemap.images[water]);
+	for(int j = 8; j < 14;j++){
+		drawImage(water.pixel_data,water.width,water.height,4*32, j*32);
 		gameStage.mapper[4][j] = 1;
 	}
-
-	for(int j = 20; j < 38; j++){
-		drawImage(j*32, (7)*32, &tilemap.images[water]);
-		gameStage.mapper[7][j] = 1;
-
+	for(int j = 0; j < 4;j++){
+		drawImage(water.pixel_data,water.width,water.height,15*32, j*32);
+		gameStage.mapper[15][j] = 1;
 	}
-
-	for(int j = 12; j< 38; j++){
-		drawImage(j*32, (10)*32, &tilemap.images[water]);
-		gameStage.mapper[10][j] = 1;
-
-	}
-
-	for(int j = 3; j< 18; j++){
-		drawImage(j*32, (13)*32, &tilemap.images[water]);
-		gameStage.mapper[13][j] = 1;
-	}
-
-	for(int j = 3; j< 27; j++){
-		drawImage(j*32, (16)*32, &tilemap.images[water]);
-		gameStage.mapper[16][j] = 1;
-	}
-
-	for(int j = 3; j< 38; j++){
-		drawImage(j*32, (19)*32, &tilemap.images[water]);
+	for(int j = 25; j < 38 ;j++){
+		drawImage(water.pixel_data,water.width,water.height,6*32, j*32);
 		gameStage.mapper[19][j] = 1;
 	}
 
-	gameStage.endi = 18*32;
-	gameStage.endj = 36*32;
-	drawImage(576, 1, &tilemap.images[level2]);
-	drawImage(gameStage.endj, gameStage.endi + 32, &tilemap.images[pointer]);
+	}
+    //holes
+	drawImage(hole.pixel_data,hole.width,hole.height,9*32, 9*32);
+	gameStage.mapper[9][9] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,12*32, 7*32);
+	gameStage.mapper[12][7] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,11*32, 9*32);
+	gameStage.mapper[11][9] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,4*32, 7*32);
+	gameStage.mapper[4][7] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,6*32, 3*32);
+	gameStage.mapper[6][3] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,12*32, 9*32);
+	gameStage.mapper[12][9] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,6*32, 15*32);
+	gameStage.mapper[6][15] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,16*32, 8*32);
+	gameStage.mapper[16][8] = 2;
+	drawImage(hole.pixel_data,hole.width,hole.height,18*32, 9*32);
+	gameStage.mapper[18][9] = 2;
+
+    //bat
+	updateBatPos();
+
+	//speed boost
+	drawSpeedBoost();
+
+	//extra lives
+	drawExtraLives();
+
+	//lives
+	for(int i = 0; i < gameStage.lives; i++)
+	{
+		drawImage(heart.pixel_data,heart.width,heart.height,i*32,0);
+	}	
+
+    //score
+	int num = gameStage.score;
+	int digit;
+	for(int i = 12; i > 9; i--)
+	{
+		digit = num % 10;
+		switch (digit) {
+			case 0:
+				drawImage(zero.pixel_data,zero.width,zero.height,i*32,0);
+				break;
+			case 1:
+				drawImage(one.pixel_data,one.width,one.height,i*32,0);
+				break;
+			case 2:
+				drawImage(two.pixel_data,two.width,two.height,i*32,0);
+				break;
+			case 3:
+				drawImage(three.pixel_data,three.width,three.height,i*32,0);
+				break;
+			case 4:
+				drawImage(four.pixel_data,four.width,four.height,i*32,0);
+				break;
+			case 5:
+				drawImage(five.pixel_data,five.width,five.height,i*32,0);
+				break;
+			case 6:
+				drawImage(six.pixel_data,six.width,six.height,i*32,0);
+				break;
+			case 7:
+				drawImage(seven.pixel_data,seven.width,seven.height,i*32,0);
+				break;
+			case 8:
+				drawImage(eight.pixel_data,eight.width,eight.height,i*32,0);
+				break;
+			case 9:
+				drawImage(nine.pixel_data,nine.width,nine.height,i*32,0);
+				break;
+		}
+		num = num / 10;
+	
+    //time
+	num = gameStage.time;
+	for(int i = 16; i > 14; i--)
+	{
+		digit = num % 10;
+		switch (digit) {
+			case 0:
+				drawImage(zero.pixel_data,zero.width,zero.height,i*32,0);
+				break;
+			case 1:
+				drawImage(one.pixel_data,one.width,one.height,i*32,0);
+				break;
+			case 2:
+				drawImage(two.pixel_data,two.width,two.height,i*32,0);
+				break;
+			case 3:
+				drawImage(three.pixel_data,three.width,three.height,i*32,0);
+				break;
+			case 4:
+				drawImage(four.pixel_data,four.width,four.height,i*32,0);
+				break;
+			case 5:
+				drawImage(five.pixel_data,five.width,five.height,i*32,0);
+				break;
+			case 6:
+				drawImage(six.pixel_data,six.width,six.height,i*32,0);
+				break;
+			case 7:
+				drawImage(seven.pixel_data,seven.width,seven.height,i*32,0);
+				break;
+			case 8:
+				drawImage(eight.pixel_data,eight.width,eight.height,i*32,0);
+				break;
+			case 9:
+				drawImage(nine.pixel_data,nine.width,nine.height,i*32,0);
+				break;
+		}
+		num = num / 10;
+	}
+
+	//finish line
+	drawImage(finish.pixel_data,finish.width,finish.height,19*32,19*32);
+	gameStage.mapper[19][19] = 3;
 
 }
-*/
 void initGameState()
 {
 	gameStage.time = 60;
